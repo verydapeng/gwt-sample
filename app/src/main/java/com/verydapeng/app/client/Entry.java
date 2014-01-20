b60b92f29c98eea6ed9515b36d841309f376195f
dapeng <dapeng@dapengliu>
2014-01-20 16:17:22 +0800

    setup done

---
+++ app/src/main/java/com/verydapeng/app/client/Entry.java
@@ -0,0 +1,16 @@
+package com.verydapeng.app.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+/**
+ *
+ * @author dapeng
+ */
+public class Entry implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        Window.alert("hello world");
+    }
+}
